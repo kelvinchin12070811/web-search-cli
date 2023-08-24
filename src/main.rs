c23cf07e54db12cf3a7b88cf15b844f9db36ffc8
Kelvin Chin <chinkaiwen@yahoo.com>
2023-08-24 22:54:06 +0800

    completed command line search feature

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::env;
+use std::process;
+
+use urlencoding::encode;
+
+fn launch_search(url: &str) {
+    process::Command::new("cmd")
+        .arg("/c")
+        .arg("start")
+        .arg(url)
+        .output()
+        .unwrap();
+}
+
+fn google_search(keyword: &str) {
+    let url = format!("https://www.google.com/search?q={}", keyword);
+    launch_search(&url.as_str());
+}
+
+fn duckduckgo_search(keyword: &str) {
+    let url = format!("https://www.duckduckgo.com/?q={}", keyword);
+    launch_search(&url.as_str());
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let service = if args.len() > 1 {
+        if args.len() >= 2 {
+            &args[1]
+        } else {
+            "g"
+        }
+    } else {
+        println!("Error, not enough params provided!");
+        process::exit(-1);
+    };
+
+    let keyword = args[2..args.len()].join(" ");
+    let keyword = encode(&keyword.as_str());
+
+    match service {
+        service if ["g", "google"].contains(&service) => google_search(&keyword),
+        service if ["d", "duckduckgo"].contains(&service) => duckduckgo_search(&keyword),
+        _ => google_search(&encode(&args[1..args.len()].join(" "))),
+    }
+}
